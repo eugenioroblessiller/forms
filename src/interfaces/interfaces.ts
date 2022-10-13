dd3a2eb1a4fdda52b757425bfe27f996b6c145ef
eugenio <eugenio@baliotech.com>
2022-10-13 16:02:22 -0500

    interface for My text input

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -4,4 +4,10 @@ export interface FormValues {
   lastName: string;
   email: string;
 }
-  
+export interface TextInputProps {
+  label: string;
+  name: string;
+  type?: 'text' | 'email' | 'password';
+  placeholder?: string;
+  [x: string]: any;
+}
